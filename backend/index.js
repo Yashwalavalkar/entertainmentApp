5de5c60eba3619ad481e006e651f44dc25986d86
yashnw <yashwalavalkar0077@gmail.com>
2024-05-31 17:52:56 +0530

    check

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port =process.env.PORT || 5000;
+const port = 8080;
 const mongoDB = require('./db');
 const cors=require('cors');
 // Connect to MongoDB
