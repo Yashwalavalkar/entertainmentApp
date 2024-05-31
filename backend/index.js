febe482c029186bafa3bac56c367e58f2213c83d
yashnw <yashwalavalkar0077@gmail.com>
2024-05-31 18:29:08 +0530

    data commited

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 const mongoDB = require('./db');
 const cors=require('cors');
 // Connect to MongoDB
