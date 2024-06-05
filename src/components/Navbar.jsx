d8b5e40332e2fe0979b5aca2f653a6ec48db4961
yashnw <yashwalavalkar0077@gmail.com>
2024-06-05 13:50:39 +0530

    new commit added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ export const Navbar = () => {
                 </Link>
               </div>
             ) : (
-              <div className="d-flex flex-wrap align-items-center">
+              <div className="d-flex align-items-center">
                 <div
                   className="btn bg-white text-success mx-1"
                   onClick={() => navigate("/about")}
